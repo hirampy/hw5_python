9d05b980eb286772294c8c5c1e2f48689e4fb0a8
hirampy <155140416+hirampy@users.noreply.github.com>
2024-04-07 22:28:10 +0300

    window_height

--- conftest.py
+++ conftest.py
@@ -6,7 +6,8 @@ from selenium import webdriver
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     browser.config.base_url = 'https://demoqa.com'
-    browser.driver.set_window_size(1080, 800)
+    browser.config.window_width = 1080
+    browser.config.window_height = 800
     browser.config.type_by_js = False
     browser.config.timeout = 5.0
 
